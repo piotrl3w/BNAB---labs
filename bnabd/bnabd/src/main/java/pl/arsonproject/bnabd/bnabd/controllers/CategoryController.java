8d729439efabea64f449b7de00c16f7cd3db55da
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@mped.pl>
2021-02-02 22:58:38 +0100

    Poprawa polaczenia do bazy danych, poprawa cofigu hibernate

--- bnabd/bnabd/src/main/java/pl/arsonproject/bnabd/bnabd/controllers/CategoryController.java
+++ bnabd/bnabd/src/main/java/pl/arsonproject/bnabd/bnabd/controllers/CategoryController.java
@@ -3,13 +3,15 @@ package pl.arsonproject.bnabd.bnabd.controllers;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.arsonproject.bnabd.bnabd.model.Category;
 import pl.arsonproject.bnabd.bnabd.repository.CategoryRepository;
 import pl.arsonproject.bnabd.bnabd.repository.HibernateUtil;
 
-@Controller
+@RestController
+@Secured("IS_AUTHENTICATED_ANONYMOUSLY")
 @RequestMapping("api/v1")
 public class CategoryController {
 
@@ -18,11 +20,15 @@ public class CategoryController {
 
     @PostMapping("/save")
     public void Save(@RequestBody Category category) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.save(category);
-        session.getTransaction().commit();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(category);
 
-        categoryRepository.save(category);
+            categoryRepository.save(category);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
